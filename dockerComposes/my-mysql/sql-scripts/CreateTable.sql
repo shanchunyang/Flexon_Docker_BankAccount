6cafa53e9e2b19affd6a87964f6300c96a06f26b
Samuel Yang <samuelyang@samuelde-mbp.lan>
2020-07-08 22:37:56 -0700

    latest

---
+++ dockerComposes/my-mysql/sql-scripts/CreateTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers(
+id VARCHAR(20),
+first_name VARCHAR(20),
+last_name VARCHAR(20),
+phone_number VARCHAR(20),
+PRIMARY KEY (id)
+);
---
+++ dockerComposes/my-mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,7 @@
+INSERT INTO customers(id, first_name, last_name, phone_number)
+VALUES ('123','Samuel', 'Yang','1234567890'),
+('124','Sam', 'Thomas','1334547890'),
+('125','Jane', 'Chen','5555567890'),
+('126','John', 'Marry','1334567890'),
+('127','Kate', 'Yang','1774567890');
+
